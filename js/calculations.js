b2221e47a3e28fb1a99198eabaf8c4174a852ff7
Mateus Gomes <gmateus987@gmail.com>
2023-09-26 16:09:18 -0300

    Creating the expressions

--- js/calculations.js
+++ js/calculations.js
@@ -1,4 +1,23 @@
-let equal = document.getElementById('equal')
- function calculate() {
+let equal = document.getElementById('equal');
+let myInput = document.getElementById('calculation');
+let preResult = document.getElementById('operation-preview');
+
+function resultPreview() {
+    alert("Funcionou")
+};
+
+
+function calculate() {
+    let newValue = myInput.value;
+    newValue = newValue.replace("-", "-");
+    newValue = newValue.replace("+", "+");
+    newValue = newValue.replace("⨉", "*");
+    newValue = newValue.replace("÷", "/");
+    newValue = newValue.replace("%", "/100");
+
     
- }
+    let result =
+    myInput.value = result;
+};
+
+equal.addEventListener('click', calculate)
